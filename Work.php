81becb8496257dc85256a4bc25f61604f47cc6e9
kolvah <kolvah_vanya@mail.ru>
2017-06-13 15:56:51 +0300

    first commit

---
+++ Work.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Как выполняеться PHP
+*/
+
+echo 'a'; //все инструкции заканчиваеться точкой с запятой ";"
+echo 'b';
+echo 'c';
+echo 2+2+2+2+2+2+2+2+2+2;
+echo 'd';
+echo 'e';
+ //константы
+define("PI", 3.141592); //define(name, value) или const NAME = 'value'
+$r = 10;
+$s = PI * $r * $r;
+echo $s;
+?>
